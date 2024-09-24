3d4c5cd13744a852259af1b8df65e0daac22b929
maezzuu <o3o_hell0@naver.com>
2024-09-24 23:26:47 +0900

    test

---
+++ js/index.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('section[id]');
+    const gnb = document.querySelectorAll('.gnb li');
+    
+    function setActiveNav() {
+        let index = sections.length;
+        
+        while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+
+        gnb.forEach((item) => {
+            item.classList.remove('on');
+        });
+        
+        if (gnb[index]) {
+            gnb[index].classList.add('on');
+        }
+    }
+
+    window.addEventListener('scroll', setActiveNav);
+    setActiveNav(); // 페이지 로드 시 초기 설정
+});
